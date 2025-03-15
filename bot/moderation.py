dd59f4d4c2e99af3c0a1331aebb1928113f92fa6
Javier Guzmán Cruz <skullowner83@gmail.com>
2025-03-14 23:59:29 -0600

    Create repeated messages protection in filter messages

--- bot/moderation.py
+++ bot/moderation.py
@@ -11,6 +11,9 @@ class Moderation():
         self.banned_words = self.moderation_config.get('banned_words')
         self.protection = self.moderation_config.get('protection')
         self.user_strikes = {}
+        self.user_messages = {}
+
+        self.user_repeated_strikes ={}
 
     async def message_filter(self, ctx):
         message = ctx.message
@@ -25,6 +28,18 @@ class Moderation():
         broadcaster_id = broadcaster_data['id']
         moderator_id = moderator_data['id']
         message_id = message.tags['id']
+
+        if self.protection.get("repeated_messages", {}).get('enable'):
+            strikes = self.protection["repeated_messages"].get("strikes")
+            reason = self.protection["repeated_messages"].get("reason")
+
+            if self.user_messages.get(user) and self.user_messages[user] == message.content:
+                self.user_repeated_strikes[user] = self.user_repeated_strikes.get(user, 0) + 1
+            
+            self.user_messages[user] = message.content
+
+            if self.user_repeated_strikes.get(user, 0) >= strikes:
+                api.set_ban(broadcaster_id, moderator_id, user_id, reason)
 
         # Check if the links protection is enable to delete the message if it contains a link
         if self.protection.get('links', {}).get('enable'):
@@ -53,7 +68,7 @@ class Moderation():
                         if self.user_strikes[user] >= strikes:
                             penalty = 'ban_user'
 
-                    match(penalty): 
+                    match(penalty):
                         case 'delete_message': 
                             api.delete_message(broadcaster_id, moderator_id, message_id)
                             if reason: await ctx.send(f"Se ha eliminado el mensaje de @{user}. {reason}")
