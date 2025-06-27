b11ed778eb19f1adbbca40fa6149b83c77aec265
Javier Guzmán Cruz <skullowner83@gmail.com>
2025-06-26 18:51:41 -0600

    fix: Get only the value of the ENUMs from the command modal.

--- view/modals/commands.py
+++ view/modals/commands.py
@@ -41,11 +41,12 @@ class CommandsModal(Modal):
         self.user_level_dropdown = DropDown(
             value=self.command.user_level or UserLevel.EVERYONE,
             options=[
-                ft.DropdownOption(key=UserLevel.EVERYONE, content=ft.Text("Everyone")),
-                ft.DropdownOption(key=UserLevel.MODERATOR, content=ft.Text("Moderator")),
-                ft.DropdownOption(key=UserLevel.SUSCRIPTOR, content=ft.Text("Suscriptor")),
-                ft.DropdownOption(key=UserLevel.BROADCASTER, content=ft.Text("Broadcaster")),
-                ft.DropdownOption(key=UserLevel.VIP, content=ft.Text("VIP"))
+                ft.DropdownOption(key=UserLevel.EVERYONE.value, content=ft.Text("Everyone")),
+                ft.DropdownOption(key=UserLevel.FOLLOWER.value, content=ft.Text("Follower")),
+                ft.DropdownOption(key=UserLevel.MODERATOR.value, content=ft.Text("Moderator")),
+                ft.DropdownOption(key=UserLevel.SUBSCRIBER.value, content=ft.Text("Subscriber")),
+                ft.DropdownOption(key=UserLevel.BROADCASTER.value, content=ft.Text("Broadcaster")),
+                ft.DropdownOption(key=UserLevel.VIP.value, content=ft.Text("VIP"))
             ]
         )
 
@@ -55,9 +56,9 @@ class CommandsModal(Modal):
             self.response_type_dropdown = DropDown(
                 value=self.command.response_type or ResponseType.SAY,
                 options=[
-                    ft.DropdownOption(key=ResponseType.SAY, content=ft.Text("Decir")),
-                    ft.DropdownOption(key=ResponseType.REPLY, content=ft.Text("Responder")),
-                    ft.DropdownOption(key=ResponseType.MENTION, content=ft.Text("Mencionar")),
+                    ft.DropdownOption(key=ResponseType.SAY.value, content=ft.Text("Decir")),
+                    ft.DropdownOption(key=ResponseType.REPLY.value, content=ft.Text("Responder")),
+                    ft.DropdownOption(key=ResponseType.MENTION.value, content=ft.Text("Mencionar")),
                 ]
             )
 
