b7d4585f8f407c30887cd096dcbc8f33bfe03248
Javier Guzman Cruz <skullowner83@gmail.com>
2025-10-20 23:45:21 -0600

    feat: change the origin for the new credentials

--- view/modals/validation.py
+++ view/modals/validation.py
@@ -1,32 +1,32 @@
 import webbrowser
 import pyperclip
 import flet as ft
+from models.appconfig import AppConfig
 from myapp import MyApp
-from utilities.token import Token
-from utilities.file import File
+from utilities import *
 from services import *
 from ..controls import *
 
 class ValidationModal(Modal):
-    def __init__(self, bot_credentials: dict, botconfig: dict) -> None:
-        self.bot_service = ServiceLocator.get('bot')
-        self.session_service = ServiceLocator.get('session')
-        self.websocket_service = ServiceLocator.get('websocket')
-        self.bot_credentials = bot_credentials
-        self.botconfig = botconfig
+    def __init__(self, credentials: dict, app_config: AppConfig) -> None:
+        self.bot_service: BotService = ServiceLocator.get('bot')
+        self.session_service: SessionService = ServiceLocator.get('session')
+        self.websocket_service: WebsocketService = ServiceLocator.get('websocket')
+        self.credentials = credentials
+        self.app_config = app_config
 
         super().__init__(
             title="¡Token inválido!",
             actions_alignment=ft.MainAxisAlignment.CENTER,
             content=self.build(),
             actions=[
-                Button(text="Abrir", width=100, on_click=lambda e: self.token_validation("OPEN")),
-                Button(text="Copiar", width=100, on_click=lambda e: self.token_validation("COPY"))
+                Button(text="Abrir", width=100, on_click=lambda e: self.token_validation('OPEN')),
+                Button(text="Copiar", width=100, on_click=lambda e: self.token_validation('COPY'))
             ]
         )
 
     def token_validation(self, mode: str):
-        token = Token(self.botconfig['client_id'], self.botconfig['client_secret'], self.botconfig['scope'], self.botconfig['redirect_uri'])
+        token = Token(self.app_config.client_id, self.app_config.client_secret, Constants.BOT_SCOPES, self.app_config.redirect_uri)
         auth_url = token.generate_auth_url()
 
         if mode == 'OPEN': webbrowser.open(auth_url)
@@ -36,11 +36,12 @@ class ValidationModal(Modal):
         new_refresh_token = token_data.get('refresh_token')
 
         if token.validation(new_token):
-            self.bot_credentials['access_token'] = new_token
-            self.bot_credentials['refresh_token'] = new_refresh_token
-            self.session_service.load_account(self.bot_credentials, self.botconfig, 'BOT')
+            self.credentials.get('bot')['access_token'] = new_token
+            self.credentials.get('bot')['refresh_token'] = new_refresh_token
+            self.bot_service.bot_credentials = self.credentials
+            self.session_service.load_account(self.credentials.get('bot'), self.app_config, AccountType.BOT)
             File.save(MyApp.credentials_path, self.session_service.serialize())
-            self.bot_service.start(self.bot_credentials, self.botconfig)
+            self.bot_service.start()
             self.page.close(self)
             self.page.go('/')
         else:
