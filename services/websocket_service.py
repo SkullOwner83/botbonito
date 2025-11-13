900cf5d42cf5872e685425fa06d84c2a91127870
Javier Guzman Cruz <skullowner83@gmail.com>
2025-11-12 23:55:13 -0600

    refactor: Change name of channel follower callback.

--- services/websocket_service.py
+++ services/websocket_service.py
@@ -11,7 +11,7 @@ class WebsocketService:
         self.session_id = None
         self.stream_online_callback = []
         self.stream_offline_callback = []
-        self.channel_follower_callback =[]
+        self.channel_follow_callback =[]
         self.channel_subscription_callback = []
         self.channel_raid_callback = []
 
@@ -55,9 +55,8 @@ class WebsocketService:
                 if meta_data.get('message_type') == 'notification':
                     if subscription_type == 'stream.online': await self.on_stream_online(payload)
                     if subscription_type == 'stream.offline': await self.on_stream_offline(payload)
-                    if subscription_type == 'channel.follow': await self.on_channel_follower(payload)
+                    if subscription_type == 'channel.follow': await self.on_channel_follow(payload)
                     if subscription_type == 'channel.subscribe': await self.on_channel_subscription(payload)
-                    if subscription_type == 'channel.raid': await self.on_channel_raid(payload)
 
             except websockets.ConnectionClosed as e:
                 print(f"Twitch websocket connection disconected: {e}")
@@ -93,14 +92,10 @@ class WebsocketService:
         for callback in self.stream_offline_callback:
             await self.run_callback(callback, payload)
 
-    async def on_channel_follower(self, payload: dict):
-        for callback in self.channel_follower_callback:
+    async def on_channel_follow(self, payload: dict):
+        for callback in self.channel_follow_callback:
             await self.run_callback(callback, payload)
     
     async def on_channel_subscription(self, payload: dict):
         for callback in self.channel_subscription_callback:
-            await self.run_callback(callback, payload)
-
-    async def on_channel_raid(self, payload: dict):
-        for callback in self.channel_raid_callback:
             await self.run_callback(callback, payload)
