654c8ed638ddf8c0a5a6b8985fae715af092b503
Javier Guzman Cruz <skullowner83@gmail.com>
2025-12-01 22:18:22 -0600

    feat: Add Mark Strike checkbox in protection modal.

--- view/modals/protection.py
+++ view/modals/protection.py
@@ -21,6 +21,7 @@ class ProtectionModal(Modal):
             ]
         )
 
+    # Show or hide controls depending the selected penalty
     def apply_penalty_state(self, value: str):
         if value == PenaltyType.TIME_OUT.value:
             self.penalty_column.col = 4
@@ -29,17 +30,37 @@ class ProtectionModal(Modal):
             self.penalty_column.col = 6
             self.duration_column.visible = False
 
+        self.mark_strike_checkbox.visible = value != PenaltyType.BAN_USER
+
+    # Update the icon of the penalty dropdown to the selected item
     def on_penalty_changed(self, e: ft.ControlEvent):
         self.penalty_dropdown.icon = ft.Icon(name=Constants.PENALTY_ICONS.get(e.control.value), color=ft.Colors.BLACK)
         self.apply_penalty_state(e.control.value)
         self.update()
+
+    # Update the values of protection instance and close the modal
+    def save_changes(self) -> None:
+        self.protection.reason = self.reason_textbox.value
+        self.protection.penalty = self.penalty_dropdown.value
+        self.protection.exclude = self.exclude_dropdown.value
+        self.protection.duration = self.duration_textbox.value
+        self.protection.strikes = self.strikes_textbox.value
+        self.protection.announce_penalty = self.announce_checkbox.value
+        self.moderation_manager.save_protections()
+        self.on_close()
+    
+    # Close the protection modal
+    def on_close(self) -> None:
+        self.page.close(self)
+        self.page.update()
 
     # Define the controls to save his reference
     def set_controls(self) -> None:
         self.reason_textbox = TextBox(value=self.protection.reason)
         self.duration_textbox = TextBox(value=self.protection.duration, one_word=True, is_numeric=True)
         self.strikes_textbox = TextBox(value=self.protection.strikes, one_word=True, is_numeric=True)
-        self.announce_checkbox = CheckBox(text='Anunciar penalización', checked=self.protection.announce_penalty)
+        self.announce_checkbox = CheckBox(text='Anunciar penalización en el chat.', checked=self.protection.announce_penalty)
+        self.mark_strike_checkbox = CheckBox(text='Marcar strike por penalización.', checked=self.protection.mark_strike)
 
         self.penalty_dropdown = DropDown(
             value=self.protection.penalty if self.protection.penalty else PenaltyType.DELETE_MESSAGE.value,
@@ -121,24 +142,15 @@ class ProtectionModal(Modal):
                             ]
                         ),
 
-                        self.announce_checkbox
+                        ft.Column(
+                            col=6,
+                            spacing=0,
+                            controls=[
+                                self.announce_checkbox,
+                                self.mark_strike_checkbox
+                            ]
+                        )
                     ]
                 )
             ]
         )
-    
-    # Update the values of protection instance and close the modal
-    def save_changes(self) -> None:
-        self.protection.reason = self.reason_textbox.value
-        self.protection.penalty = self.penalty_dropdown.value
-        self.protection.exclude = self.exclude_dropdown.value
-        self.protection.duration = self.duration_textbox.value
-        self.protection.strikes = self.strikes_textbox.value
-        self.protection.announce_penalty = self.announce_checkbox.value
-        self.moderation_manager.save_protections()
-        self.on_close()
-    
-    # Close the protection modal
-    def on_close(self) -> None:
-        self.page.close(self)
-        self.page.update()
