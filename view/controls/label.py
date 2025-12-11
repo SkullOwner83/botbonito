977a72797de658e31d3581aea465b484b0562235
Javier Guzman Cruz <skullowner83@gmail.com>
2025-12-10 23:49:38 -0600

    refactor: remove unnecessary Optional typing

--- view/controls/label.py
+++ view/controls/label.py
@@ -3,11 +3,11 @@ import flet as ft
 from myapp import MyApp
 
 class Label(ft.Text):
-    def __init__(self, text:str, color: Optional[ft.Colors] = ft.Colors.BLACK, **kwargs) -> None:
+    def __init__(self, text:str, color: ft.Colors = ft.Colors.BLACK, weigth: ft.FontWeight = ft.FontWeight.BOLD, **kwargs) -> None:
         super().__init__(
             value=text,
             font_family=MyApp.font_primary,
-            weight=ft.FontWeight.BOLD,
+            weight=weigth,
             color=color,
             size=16,
             **kwargs
